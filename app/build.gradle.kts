f8670e5245b3edd97e5ea3faffa54bbfcc340b9b
tranhaidangdz <141541516+tranhaidangdz@users.noreply.github.com>
2025-08-19 08:27:03 +0700

    thêm icon app và splash screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 }
 
 dependencies {
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -48,6 +49,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+//    áp dụng cho spash screen
+    implementation("androidx.core:core-splashscreen:1.0.1")
     implementation(libs.androidx.ui.text.google.fonts)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
