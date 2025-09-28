1be5581a02bbcb0fdf6bd2cd44e5dd21603fbfa9
DinhNgocTheApero <thedn@talent.apero.vn>
2025-09-28 19:04:42 +0700

    Setup base project, start with OnboardingScreen.kt

---
+++ app/src/main/java/com/utc/driverxy/navigation/Destination.kt
@@ -0,0 +1,12 @@
+package com.utc.driverxy.navigation
+
+import androidx.navigation3.runtime.NavKey
+import kotlinx.serialization.Serializable
+
+@Serializable
+sealed interface Destination : NavKey {
+    @Serializable
+    data object Welcome : Destination
+    @Serializable
+    data object Onboarding : Destination
+}
